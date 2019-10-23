5886dc79b985b9ba31cdbaebf658151e8c14eb38
Sean Lingren <sean@lingrino.com>
2019-10-22 17:35:06 -0700

    Brew formula update for vaku version v1.3.0

--- Formula/vaku.rb
+++ Formula/vaku.rb
@@ -2,16 +2,16 @@
 class Vaku < Formula
   desc "A CLI and Go API to extend the official Vault client"
   homepage "https://vaku.io/"
-  version "1.2.0"
+  version "1.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lingrino/vaku/releases/download/v1.2.0/vaku_1.2.0_Darwin_x86_64.tar.gz"
-    sha256 "6467334520250162df7e78b231e261ed9604c307b4acabeea7c64340482d0f6d"
+    url "https://github.com/lingrino/vaku/releases/download/v1.3.0/vaku_1.3.0_Darwin_x86_64.tar.gz"
+    sha256 "0850c6d6ad94e861cbaba98783b399af5b12a7cd879fcddd2fa84600968697e9"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lingrino/vaku/releases/download/v1.2.0/vaku_1.2.0_Linux_x86_64.tar.gz"
-      sha256 "19dc20f1b3c48562aee65d542b837d8060ccfa8312999c6d4c6ae61577522158"
+      url "https://github.com/lingrino/vaku/releases/download/v1.3.0/vaku_1.3.0_Linux_x86_64.tar.gz"
+      sha256 "c74f507538c756153ff9548f36e4d65cdd524cb44d7869861d069b4aa5fe3eeb"
     end
   end
   
