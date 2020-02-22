771b8e79f207adc2ffd2ce453c763d893cad90b4
Sean Lingren <sean@lingrino.com>
2020-02-21 23:31:44 -0500

    Brew formula update for glen version v1.1.2

--- Formula/glen.rb
+++ Formula/glen.rb
@@ -2,16 +2,16 @@
 class Glen < Formula
   desc "A CLI to export GitLab project and group variables"
   homepage ""
-  version "1.1.1"
+  version "1.1.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lingrino/glen/releases/download/v1.1.1/glen_1.1.1_Darwin_x86_64.tar.gz"
-    sha256 "ece6d38f5962ca780cbe503dfacd6307de800be245ce07239d1aed5bdf786b35"
+    url "https://github.com/lingrino/glen/releases/download/v1.1.2/glen_1.1.2_Darwin_x86_64.tar.gz"
+    sha256 "91f309d4fac9b22ad8c74548f05f2cff95fff4821877c8749671a2f7a970b7a0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lingrino/glen/releases/download/v1.1.1/glen_1.1.1_Linux_x86_64.tar.gz"
-      sha256 "5a3f201c9e59738ee53ea1e5da440dd3aa3b4c42946f30d7c52fdaa5d58de4dd"
+      url "https://github.com/lingrino/glen/releases/download/v1.1.2/glen_1.1.2_Linux_x86_64.tar.gz"
+      sha256 "39995d47618fe2be1a6e474f45cc990f1f909acc5fa35eaa5de82a9f597edc99"
     end
   end
 
