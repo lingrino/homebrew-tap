e7147a1909339bd1eda198fd084ddb2c89559b9c
Sean Lingren <sean@lingrino.com>
2020-10-31 14:40:11 -0700

    Brew formula update for cami version v1.0.4

--- Formula/cami.rb
+++ Formula/cami.rb
@@ -2,24 +2,24 @@
 class Cami < Formula
   desc "A CLI for cleaning up unused AWS AMIs."
   homepage "https://lingrino.com/"
-  version "1.0.3"
+  version "1.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/lingrino/cami/releases/download/v1.0.3/cami_1.0.3_mac_x86_64.zip"
-    sha256 "a19f59ec228b98eae4fe5341839594d6cc5f13e471fd9cc48b31c89dbbdc7f82"
+    url "https://github.com/lingrino/cami/releases/download/v1.0.4/cami_1.0.4_mac_x86_64.zip"
+    sha256 "079ade675eeca4c4b5c28e7109d6ea0051e76040cc3bf84f6bf4b44944194758"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/lingrino/cami/releases/download/v1.0.3/cami_1.0.3_linux_x86_64.zip"
-      sha256 "6bf3bdf63b856fba3553ac016fcd86c39336794c4803e752212d7583b5314888"
+      url "https://github.com/lingrino/cami/releases/download/v1.0.4/cami_1.0.4_linux_x86_64.zip"
+      sha256 "19e4d7a4d90d22a4b232501ddf15e1a3342603413efa4a405b59f2ef5d35df9a"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/lingrino/cami/releases/download/v1.0.3/cami_1.0.3_linux_arm64.zip"
-        sha256 "e8dc33d9c45ebadc90c08eb2ec5031eaa8cb20cb39819278ecf03502b0adbd08"
+        url "https://github.com/lingrino/cami/releases/download/v1.0.4/cami_1.0.4_linux_arm64.zip"
+        sha256 "aed81cd37560be6adf4f9e00a8c5f341dba0270d9ddd2e0b29dc2dd923f2ca6c"
       else
-        url "https://github.com/lingrino/cami/releases/download/v1.0.3/cami_1.0.3_linux_armv6.zip"
-        sha256 "213e1cab624045d5dafa0ff09b13263cc21831cb940e3652cb20ac3c5de27515"
+        url "https://github.com/lingrino/cami/releases/download/v1.0.4/cami_1.0.4_linux_armv6.zip"
+        sha256 "a3d84c990d770c719b5c40488e617b56f712502f6abd320022fa2652c0274777"
       end
     end
   end
